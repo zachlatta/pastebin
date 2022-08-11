7e933e27f8e55dc18daddb413e62050828b1ac65
Zach Latta <zach@zachlatta.com>
2022-08-11 11:26:58 +0000

    Return /

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,21 @@ extern crate rocket;
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build()
+    rocket::build().mount("/", routes![index])
+}
+
+#[get("/")]
+fn index() -> &'static str {
+    "
+    USAGE
+    
+        POST /
+        
+            accepts raw data in the body of the request and responds with a URL of
+            a page containing the body's content
+        
+        GET /<id>
+        
+            retrieves the content for the paste with id `<id>`
+    "
 }
